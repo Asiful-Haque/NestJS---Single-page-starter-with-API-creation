12296f271ff71f2a8f7dcbed58f20f848dc8d4b1
Asiful Haque <73637081+Asiful-Haque@users.noreply.github.com>
2025-04-28 19:51:55 +0600

    Update heros.module.ts

--- src/heros/heros.module.ts
+++ src/heros/heros.module.ts
@@ -12,3 +12,35 @@ import { HerosService } from './heros.service';
   providers: [HerosService],
 })
 export class HerosModule {}
+
+
+
+if want to add middleware
+// src/middleware/logger.middleware.ts
+import { Request, Response, NextFunction } from 'express';
+
+export function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
+  console.log('Request URL:', req.url);
+  next();
+}
+
+
+// src/app.module.ts
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { loggerMiddleware } from './middleware/logger.middleware';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Hero.name, schema: HeroSchema }]),
+  ],
+  controllers: [HerosController],
+  providers: [HerosService],
+})
+  
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(loggerMiddleware) // ✅ function middleware
+      .forRoutes('*');         // ✅ apply for all routes
+  }
+}
